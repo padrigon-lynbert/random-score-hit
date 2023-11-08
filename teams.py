9093cd287aed6530443bffc61a08d01a75378be4
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2023-11-08 16:49:52 +0800

    format

--- teams.py
+++ teams.py
@@ -1,58 +1,61 @@
+import matplotlib.pyplot as plt
+from MeanMedianMode.math_algorithms.mean import mean
 import random
 import time
 red, blue = list(), list()
-RED, BLUE = [],[]
+RED, BLUE = [], []
 
 while True:
     hit = random.choice([3, 4, 5])
-    
     score_to = random.choice(['Red', 'Blue'])
 
     if score_to == "Red":
         try:
-            red.append(hit); RED.append(sum(red)+hit); BLUE.append(BLUE[-1])
-        except IndexError: BLUE.append(0)
-        
+            red.append(hit)
+            RED.append(sum(red)+hit)
+            BLUE.append(BLUE[-1])
+        except IndexError:
+            BLUE.append(0)
+
         print(f"{score_to}: {sum(red)}")
-        # time.sleep(.5)
+        time.sleep(.5)
+
         if sum(red) >= 50:
             break
+
     else:
+
         try:
-            blue.append(hit); BLUE.append(sum(blue)+hit); RED.append(RED[-1])
-        except IndexError: RED.append(0)
+            blue.append(hit)
+            BLUE.append(sum(blue)+hit)
+            RED.append(RED[-1])
+        except IndexError:
+            RED.append(0)
+
         print(f"{score_to}: {sum(blue)}")
-        # time.sleep(.5)
+        time.sleep(.5)
+
         if sum(blue) >= 50:
             break
-if(sum(red) > sum(blue)):
-    print(f"\nred: {sum(red)} (winner)")
-else:
-    print(f"\nblue: {sum(blue)} (winner)")
 
-from MeanMedianMode.math_algorithms.mean import mean
+if (sum(red) > sum(blue)): print(f"\nred: {sum(red)} (winner)")
+else: print(f"\nblue: {sum(blue)} (winner)")
+
 
 print("MATCH INFO")
-# ave = round(mean([3, 5, 4, 5, 4, 3, 5, 3, 3, 3, 3, 3, 4, 5]),1)
 print(f"Average score points: |Red: {round(mean(red),1)} vs {round(mean(blue),1)} : Blue|")
 
 
-import matplotlib.pyplot as plt
-
-redplot, blueplot = [],[]
-print(BLUE)
-plt.plot(RED,label="red", color="red")
+plt.plot(RED, label="red", color="red")
 plt.plot(BLUE, label="blue", color="blue")
 plt.xlabel('Time')
 plt.ylabel('Score')
 
-plt.title("Teams")
+plt.title(f"Teams\nMatch Info: ")
 
 plt.legend()
-# plt.text(0,f"hello")
-
 plt.text(0, RED[0], sum(red), fontsize=12, color='red', ha='left', va='bottom')
-plt.text(0, BLUE[0], sum(blue), fontsize=12, color='blue', ha='left', va='bottom')
+plt.text(0, BLUE[0], sum(blue), fontsize=12,
+         color='blue', ha='left', va='bottom')
 
 plt.show()
-
